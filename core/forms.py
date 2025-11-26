6cbada23751ace7759a40c9ae19ca35cb2eb604d
walgak <118623263+walgak@users.noreply.github.com>
2025-11-26 17:39:38 +0300

    Minor bug fixes

--- core/forms.py
+++ core/forms.py
@@ -10,6 +10,6 @@ class InquiryForm(forms.ModelForm):
             'first_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'John'}),
             'last_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Doe'}),
             'email': forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'john@example.com'}),
-            'project_type': forms.Select(attrs={'class': 'form-input', 'style': 'cursor: pointer;'}),
+            'project_type': forms.Select(attrs={'class': 'form-input', 'style': 'cursor: pointer;','style': 'background-color: #2563eb',}),
             'details': forms.Textarea(attrs={'class': 'form-input', 'rows': 6, 'placeholder': 'Describe your core features, target audience, and goals...'}),
         }
